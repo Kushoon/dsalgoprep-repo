11bdef18d875ab3779147792ee746e2f843afd8e
kushoon <abhishek.serjam@gmail.com>
2021-11-09 23:46:04 +0800

    palindrome changes

--- 3-palindrome.js
+++ 3-palindrome.js
@@ -1,2 +1,15 @@
 // Given an integer return true if x is a palndrome integer or else return false
 // Simple method - convert to string and then loop through each
+var isPalindrome = function (x) {
+  x = x.toString();
+  let stoppingPoint = Math.floor(x.length / 2);
+  for (let i = 0; i < stoppingPoint; i++) {
+    if (x[i] != x[x.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Another way could be to take the number from the half point - reverse it and compare it to the first half
+// 1221 - 21, reverse this to 12 and compare with first half
